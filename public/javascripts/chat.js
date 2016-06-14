556841e286562d2fe6d52561315a89dd5283f129
costajeanpierre <dasilva.j.pierre@gmail.com>
2016-06-14 16:56:41 +0200

    filter init

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,6 +1,7 @@
 var sendButton = document.getElementById('send-message');
 var playerMessage = document.getElementById('player-message');
 var messagesContainer = document.getElementById('messages');
+var filters = document.getElementsByClassName('filters');
 
 function sendMessage() {
     var message = playerMessage.value;
@@ -13,6 +14,13 @@ function sendMessage() {
 sendButton.addEventListener('click', function (e) {
     sendMessage();
 });
+
+for (var i = 0; i < filters.length; i++) {
+    filters[i].addEventListener('click', function (e) {
+        socket.emit('chat-filter', this.id);
+    });
+}
+
 window.addEventListener('keyup', function (e) {
     if (e.keyCode == "13") { // ENTER
         sendMessage();
@@ -21,8 +29,23 @@ window.addEventListener('keyup', function (e) {
 
 socket.emit('chat-is-writing');
 
-socket.on('receive-message', function (playerName, message, time) {
+function addMessage(from, message, time) {
     var p = document.createElement('p');
-    p.innerHTML = time + ' ' + playerName + ' => ' + message;
+    p.innerHTML = time + ' ' + from + ' => ' + message;
     messagesContainer.appendChild(p);
+}
+
+socket.on('receive-message', function (playerName, message, time) {
+    addMessage(playerName, message, time);
+});
+
+socket.on('chat-filter', function (messages) {
+    messagesContainer.removeChildren();
+    messages.forEach(function (message) {
+        var from = message.sender,
+            mess = message.message,
+            time = message.time;
+            
+        addMessage(from, mess, time);
+    });
 });
