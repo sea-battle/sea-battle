c1e452fee8a9a23cacdbf180048e58ad6fa54281
louisfisch <louis.fischer@me.com>
2016-06-14 12:17:09 +0200

    Set up sessions

--- app.js
+++ app.js
@@ -1,46 +1,34 @@
 var app = require('express')(),
+    bodyParser = require('body-parser'),
 	express = require('express'),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose'),
-	passport = require('passport');
-    session = require('express-session');
+    io = require('socket.io').listen(server),
+    mongoose = require('mongoose'),
+    morgan = require('morgan'),
+	server = require('http').createServer(app);
 
 var game = require('./game');
 
 var config = require('./config');
 
-var routeAuthentication = require('./routes/authentication');
+app.use(morgan('dev'));
 
+// Get informations from HTML forms. Needs to be loaded before routesAuthentication
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// In a Connect or Express-based application, passport.initialize() middleware is required to initialize Passport.
-app.use(passport.initialize());
-
-// Sessions settings
-app.use(session({
-    cookie: {
-        maxAge: 60000
-    },
-    secret: 'sea-battle-is-the-shit',
-    resave: false,
-    saveUninitialized: false
-}));
+// Load all routes and middlewares required for authentication
+var routesAuthentication = require('./routes/authentication');
+app.use('/', routesAuthentication);
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
-
-app.use('/', routeAuthentication);
 
 app.get('/database', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-	//game.roomToJoin = '/tamere';
 	res.render(__dirname + '/views/index');
 });
 app.get('/rooms', function (req, res) {
@@ -52,25 +40,14 @@ app.get('/wait', function (req, res) {
 app.get('/game', function (req, res) {
 	res.render(__dirname + '/views/game');
 });
-app.get('/signup', function (req, res) {
-    res.render(__dirname + '/views/signup');
-});
-app.get('/manage-account', function (req, res) {
-    res.render(__dirname + '/views/manage-account');
-});
 app.get('*', function (req, res) {
 	res.render(__dirname + '/views/404');
 });
 
-// Passport configuration
-var User = require('./models/user');
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
 mongoose.connect(config.database.location, config.database.options);
 
 io.sockets.on('connection', function (socket) {
-	// INIT ON CONNECTION
+	// Init on connection
 	game.rooms['test'] = {
 		timer: game.defaultPlacementTime,
 		timerId: null
@@ -80,7 +57,7 @@ io.sockets.on('connection', function (socket) {
 	socket.name = socket.id;
 	socket.join(socket.room);
 
-	// WAIT STAGE
+	// Wait stage
 	socket.on('wait-set-ready', function () {
 		socket.ready = true;
 		if (game.allPlayersAreReady(io.sockets, socket.room) &&
@@ -91,7 +68,6 @@ io.sockets.on('connection', function (socket) {
 					game.rooms[socket.room].timer = game.defaultShootTime;
 					// Check if player grid is ready
 					io.sockets.emit('game-check-grid');
-					//io.sockets.emit('game-start-battle');
 					clearInterval(game.rooms[socket.room].timerId);
 				} else {
 					io.sockets.emit('game-timer-update', game.rooms[socket.room].timer);
@@ -109,57 +85,7 @@ io.sockets.on('connection', function (socket) {
 	socket.on('game-set-ready', function (cells) {
 		socket.cells = cells;
 		socket.emit('game-init-players-grids', game.getPlayersNames(io.sockets, socket.room));
-		//game.getPlayersCells(io.sockets, socket.room);
 	});
-
-
-
-	/* ROOMS
-    socket.on('createRoom', function (name){
-        game.rooms.push(name);
-        socket.room = name;
-        socket.join(name);
-        io.sockets.emit('updateRooms', game.rooms);
-        socket.join(name);
-        socket.emit('joinRoom');
-    });
-
-    socket.on('getRooms', function (){
-        socket.emit('updateRooms', game.rooms);
-    });
-
-    socket.on('fromWait', function (){
-        console.log(socket.room);
-    });
-	*/
-	/* TEST
-	console.log(io);
-	//socket.on('init', function (data) {});
-	if (game.players.length < game.ROOM_MAX_PLAYER &&
-	    game.roomToJoin != '/') {
-	    socket.player = {
-	        ready: false
-	    };
-	    game.players.push(socket);
-
-	    socket.on('setReady', function () {
-	        if (!socket.player.ready) {
-	            socket.player.ready = true;
-	            if (game.getPlayerReadyCount() == game.players.length &&
-	                game.players.length > 1) {
-	                io.sockets.emit('startGame', socket.player);
-	            }
-	        }
-	    });
-	}
-
-	socket.on('disconnect', function () {
-	    var i = game.players.indexOf(socket);
-	    game.players.splice(i, 1);
-	});
-
-	console.log(game.roomToJoin + ' => ' + 'game.players.length:', game.players.length);
-	*/
 });
 
 server.listen(3000);
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,13 +1,45 @@
+var cookieParser = require('cookie-parser');
 var express = require('express');
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
 
 var User = require('../models/user');
 
 var router = express.Router();
 
+// Read cookies
+router.use(cookieParser());
+
+// Passport configuration
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
 passport.use(new LocalStrategy(User.authenticate()));
 
+// Sessions settings
+router.use(session({
+    cookie: {
+        maxAge: 1000 * 3600
+    },
+    resave: false,
+    saveUninitialized: true,
+    secret: 'sea-battle-is-the-shit',
+}));
+
+// In a Connect or Express-based application, passport.initialize() middleware is required to initialize Passport.
+router.use(passport.initialize());
+
+router.use(passport.session());
+
+// Routes: method POST
 router.post('/check-username-availability', function (req, res) {
     User.findOne({ username: req.body.username }, function (err, user) {
         if (user) {
@@ -26,49 +58,72 @@ router.post('/check-username-availability', function (req, res) {
 
 router.post('/signup', function (req, res) {
     User.register(new User({
-        username: req.body.username,
-        email: req.body.email,
-        validated: false
-    }), req.body.password, function (err, user) {
-        if (err) {
-          return res.json(403, {
-              signupSucceed : false,
-              signupMessage : 'Une erreur est survenue lors de l\'inscription'
-          });
-        }
+            username: req.body.username,
+            email: req.body.email,
+            validated: false
+        }),
+        req.body.password, function (err, user) {
+            if (err) {
+                return res.status(403).send({
+                    signupSucceed: false,
+                    signupMessage: 'Une erreur est survenue lors de l\'inscription'
+                });
+            }
 
-        passport.authenticate('local', { failureFlash: true })(req, res, function () {
-            res.json(200, {
-                signupSucceed : true,
-                signupMessage : 'Un e-mail de confirmation vous a été envoyé'
+            passport.authenticate('local', {
+                failureFlash: true
+            })(req, res, function () {
+                return res.status(200).send({
+                    signupSucceed: true,
+                    signupMessage: 'Un e-mail de confirmation vous a été envoyé'
+                });
             });
-        });
-    });
+        }
+    );
 });
 
-router.post('/signin', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
+router.post('/signin', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
         if (err) {
             return next(err);
         }
 
         if (!user) {
-            return res.send(401, {
+            return res.status(401).send({
                 signinSuccess: false,
                 signinMessage: 'Le pseudonyme ou le mot de passe est incorrect'
             });
         }
 
-        req.login(user, function(err) {
+        req.login(user, function (err) {
             if (err) {
                 return next(err);
             }
-            return res.send(200, {
+            return res.status(200).send({
                 signinSuccess: true,
                 signinMessage: 'Vous êtes maintenant connecté en tant que ' + user.username
             });
         });
     })(req, res, next);
+});
+
+// Route middleware: make sure the user is authenticated
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.redirect('/YOLO');
+    }
+    else {
+        return next();
+    }
+}
+
+// Routes: method GET
+router.get('/signup', isAuthenticated, function (req, res) {
+    res.render(__dirname + '/../views/signup');
+});
+
+router.get('/manage-account', function (req, res) {
+    res.render(__dirname + '/../views/manage-account');
 });
 
 router.get('/signout', function (req, res) {
