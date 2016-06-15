0dfe982c25bab12e1597c718c09a8da8027e2562
costajeanpierre <dasilva.j.pierre@gmail.com>
2016-06-15 16:40:37 +0200

    wait update

--- public/javascripts/pages/wait.js
+++ public/javascripts/pages/wait.js
@@ -1,37 +1,53 @@
 var playerIsReady = false;
 var setReadyButton = document.getElementById('set-ready');
 var readyStatus = document.getElementById('ready-status');
+var playersList = document.getElementById('players-list');
 
 setReadyButton.addEventListener('click', function (e) {
-	playerIsReady = !playerIsReady;
-	if (playerIsReady){
-		readyStatus.innerHTML = 'Ready';
-		socket.emit('wait-set-ready');
-	}else{
-		readyStatus.innerHTML = '';
-		socket.emit('wait-set-unready');	
-	}
+    playerIsReady = !playerIsReady;
+    if (playerIsReady) {
+        readyStatus.innerHTML = 'Ready';
+        socket.emit('wait-set-ready');
+    } else {
+        readyStatus.innerHTML = '';
+        socket.emit('wait-set-unready');
+    }
 });
 
 socket.on('wait-start-game', function () {
-	ajax.get('/game', handlers);
+    ajax.get('/game', handlers);
 });
 
-socket.on('room-update', function (playerName) {
-	console.log(playerName);
-    var li = document.createElement('li');
-    var div = document.createElement('div');
-    var pUsername = document.createElement('p');
-    var pRanking = document.createElement('p');
-    var buttonReady = document.createElement('button');
-    var iStatus = document.createElement('i');
-    
-    pUsername.className = 'player-username';
-    pRanking.className = 'player-ranking';
-    buttonReady.className = 'button-effect';
-    iStatus.className = 'ready-status';
-    
-    /*
-    <li><div><p class="player-username">Pseudo</p><p class="player-ranking">Image Grade X pts</p></div><button id="set-ready" class="button-effect">Prêt<i id="ready-status"></i></button></li>
-    */
+socket.on('room-update-players', function (playersName) {
+    console.log(socket);
+    var indexToRemove = playersName.indexOf(socket.name);
+    playersName.splice(indexToRemove, 1);
+
+    var otherPlayersRow = document.getElementsByClassName('other-player');
+    for (var i = 0; i < otherPlayersRow.length; i++) {
+        otherPlayersRow[i].remove();
+    }
+
+    playersName.forEach(function (playerName) {
+        var li = document.createElement('li');
+        var div = document.createElement('div');
+        var pUsername = document.createElement('p');
+        var pRanking = document.createElement('p');
+        var iStatus = document.createElement('i');
+        li.id = playerName + '-row';
+        li.className = 'other-player';
+        pUsername.className = 'player-username';
+        pRanking.className = 'player-ranking';
+        iStatus.id = 'status-' + playerName;
+
+        pUsername.innerHTML = playerName;
+        pRanking.innerHTML = 'Essaye pas de niaiser !';
+        iStatus.innerHTML = 'Attente';
+
+        li.appendChild(div);
+        div.appendChild(pUsername);
+        div.appendChild(pRanking);
+        li.appendChild(iStatus);
+        playersList.appendChild(li);
+    });
 });
