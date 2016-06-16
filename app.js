b90d65dd1763b2d5fe46aeb87abf314be80f5011
costajeanpierre <dasilva.j.pierre@gmail.com>
2016-06-16 09:06:06 +0200

    left game bug fixed

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ io.sockets.on('connection', function (socket) {
         });
         socket.broadcast.emit('receive-message', from, message, time, filter);
         var playersName = game.getPlayersNames(io.sockets, socket.room);
-        io.sockets.broadcast.emit('room-update-players', playersName);
+        //io.sockets.broadcast.emit('room-update-players', playersName);
     });
 
     // Stage 2: wait
