3d86b2d1fe4e04a0618db4c5461ae6e917b449a5
Keylies <lemenct@gmail.com>
2016-06-21 22:05:57 +0200

    Tutorial page added

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var jade = require('jade');
 
+router.get('/tutorial', function (req, res) {
+	res.render(__dirname + '/../views/tutorial', {
+		bodyClass: 'tutorial'
+	});
+});
+
 router.get('*', function (req, res) {
 	res.render(__dirname + '/../views/404', {
 		bodyClass: '404'
